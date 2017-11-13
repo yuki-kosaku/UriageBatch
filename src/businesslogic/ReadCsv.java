c416b1c83f7e88dfe577e6669ca6ff67f0227eb7
takeshi-kida <32971757+takeshi-kida@users.noreply.github.com>
2017-11-13 22:15:39 +0900

    ReadCsv汎用化

--- src/businesslogic/ReadCsv.java
+++ src/businesslogic/ReadCsv.java
@@ -1,9 +1,11 @@
 package businesslogic;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import businessEntity.dao.InsertT_SALE_TRAN;
@@ -11,8 +13,44 @@ import businessEntity.dto.T_SALE_TRAN;
 
 public class ReadCsv {
 
-	public void readUriageCsv() throws Exception
-	{
+	public ArrayList<ArrayList<String>> readCsv(String fileName) {
+		ArrayList<ArrayList<String>> raw = new ArrayList<>();
+
+		try {
+			File csv = new File(fileName);
+
+			BufferedReader br = new BufferedReader(new FileReader(csv));
+
+			// 最終行まで読み込む
+			String line = "";
+			while ((line = br.readLine()) != null) {
+
+				// 1行をデータの要素に分割
+				StringTokenizer st = new StringTokenizer(line, ",");
+
+				ArrayList<String> col = new ArrayList<>();
+				while (st.hasMoreTokens()) {
+					// 1行の各要素をカンマ区切りで表示
+					col.add(st.nextToken());
+
+				}
+				raw.add(col);
+			}
+
+			br.close();
+		} catch (FileNotFoundException e) {
+			// Fileオブジェクト生成時の例外捕捉
+			e.printStackTrace();
+		} catch (IOException e) {
+			// BufferedReaderオブジェクトのクローズ時の例外捕捉
+			e.printStackTrace();
+		}
+
+		return raw;
+
+	}
+
+	public void readUriageCsv() throws Exception {
 		try {
 			File csv = new File("C:\\TEMP\\URIAGE.csv"); // CSVデータファイル
 
@@ -38,7 +76,7 @@ public class ReadCsv {
 				while (st.hasMoreTokens()) {
 					// 1行の各要素をタブ区切りで表示
 
-					tSaleTran.VOUCHER_NO =	st.nextToken();
+					tSaleTran.VOUCHER_NO = st.nextToken();
 					tSaleTran.PRODUCT_CD = st.nextToken();
 					tSaleTran.SALES = Integer.parseInt(st.nextToken());
 				}
@@ -56,8 +94,7 @@ public class ReadCsv {
 		}
 	}
 
-	public void readShiireCsv() throws Exception
-	{
+	public void readShiireCsv() throws Exception {
 		try {
 			File csv = new File("C:\\TEMP\\SHIIRE.csv"); // CSVデータファイル
 
@@ -81,22 +118,21 @@ public class ReadCsv {
 				while (st.hasMoreTokens()) {
 					// 1行の各要素をタブ区切りで表示
 
-					costYMD =	st.nextToken();
+					costYMD = st.nextToken();
 					productCd = st.nextToken();
 					costs = Integer.parseInt(st.nextToken());
 					costAmount = costs * selectOracle.selectCostUnitPrice(costYMD, productCd);
 				}
 
-				insertOracle.insertShiire("INSERT INTO T_COST values(?, ?, ?, ?)", costYMD, productCd, costs, costAmount);
+				insertOracle.insertShiire("INSERT INTO T_COST values(?, ?, ?, ?)", costYMD, productCd, costs,
+						costAmount);
 
 				// 在庫に商品がない場合、在庫に登録する。
-				if(selectOracle.selectZaikoRecord(productCd) == 0)
-				{	
+				if (selectOracle.selectZaikoRecord(productCd) == 0) {
 					insertOracle.insertZaiko("INSERT INTO T_STOCK values(?, ?, ?)", costYMD, productCd, costs);
-				}
-				else
-				{
-					insertOracle.updateZaikoFromCost("UPDATE T_STOCK SET STOCKS = STOCKS + " + costs  + " WHERE STOCK_YMD = '" + costYMD + "' AND PRODUCT_CD = '" + productCd + "'" );					
+				} else {
+					insertOracle.updateZaikoFromCost("UPDATE T_STOCK SET STOCKS = STOCKS + " + costs
+							+ " WHERE STOCK_YMD = '" + costYMD + "' AND PRODUCT_CD = '" + productCd + "'");
 				}
 			}
 			br.close();
